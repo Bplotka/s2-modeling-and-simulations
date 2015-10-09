b1f3df261aa6de5b311496639690ca849fdd976a
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2015-10-09 08:15:01 -0400

    First function added.
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 LD_LIBRARY_PATH="/usr/local/lib" g++ exercise1.cpp -lgmp -lgmpxx -o gites
+chmod +x ./gites
--- exercise1.cpp
+++ exercise1.cpp
@@ -11,21 +11,34 @@
 
 using namespace std;
 
-mpf_class meanVal(const mpf_class* arri, int n,int d) {
+mpf_class meanVal(const mpf_class* arr, long size, long acc) {
         
-  mpf_class sum("0",d);
-  mpf_class length(n,d);
+  mpf_class sum(0, acc);
+  mpf_class length(size, acc);
   for (int i=0;i<length;i++)
   {
-        sum += arri[i];
+        sum += arr[i];
   }
-   mpf_class result("0",64);
+   mpf_class result(0, acc);
    result = sum/length;
    return result;
 }
 
+mpf_class varVal(const mpf_class* arr, long size, long acc) {
 
-const vector<mpf_class> getStream() {
+  mpf_class sum(0, acc);
+  mpf_class length(size, acc);
+  for (int i=0;i<length;i++)
+  {
+    sum += arr[i];
+  }
+  mpf_class result(0, acc);
+  result = sum/length;
+  return result;
+}
+
+
+const vector<mpf_class> getStream(long acc) {
   vector<mpf_class> vec;
 
   string line;
@@ -39,6 +52,7 @@ const vector<mpf_class> getStream() {
   {
     mpf_class input_val;
     if(!(lineStream >> input_val)) break;
+    input_val.set_prec(acc);
 
     vec.push_back(input_val);
   }
@@ -63,9 +77,10 @@ void printUsage(char **argv) {
 
 
 int main (int argc, char **argv) {
+  // Max acc: 2^16 = 65536
   // Max size of array: 2^24 = 16777216
   // Max elem | value |: 2^64 = 18446744073709551616
-  int accuracy = 9999; // Default accuracy.
+  int accuracy = 65536; // Default accuracy.
 
   if ( argc > 1 ) {
     if (!strcmp(argv[1], "-h")) {
@@ -77,26 +92,16 @@ int main (int argc, char **argv) {
     accuracy = atoi(argv[1]);
   }
 
-  vector<mpf_class> vec = getStream();
+  vector<mpf_class> vec = getStream(accuracy);
 
-  debugPrintArr(&vec[0], vec.size());
+//debugPrintArr(&vec[0], vec.size());
 
-  // arg1 - accuracy (d) 1 <= d <= 2^16
-  // Ciag
-  mpf_class a("2",64), b("1",64), c("1",64);
-  //a = 2;
-  //b = "3";
-  //c = a/b;
-  mpf_class arri[3] = {a, b, c};
-//  arri[0]=a;
-//  arri[1]=b;
-//  arri[2]=c;
-// cout << "mean " << meanVal(arri,3,64)<< "\n";
-  
- gmp_printf ("fixed point mpf %.*Ff with %d digits\n", 64, meanVal(arri,3,64), 64);
-  //cout << "absolute value is " << b.get_str() << "\n";
-  //int n =64;
-//  gmp_printf ("fixed point mpf %.*Ff with %d digits\n", n, b, n);
+  cout.precision(accuracy);
+  cout << meanVal(&vec[0], 3, accuracy) << "\n";
+
+  cout << varVal(&vec[0], 3, accuracy) << "\n";
+
+  //cout << meanVal(&vec[0], 3, accuracy) << "\n";
 
   return 0;
 }
