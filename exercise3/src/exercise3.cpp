708155156e0a66c917c913483eabccb35294a6b9
pprokop <pprokop@localhost.localdomain>
2015-11-17 23:31:45 +0100

    Refactoring due to BPlotka's comments

--- exercise3/src/exercise3.cpp
+++ exercise3/src/exercise3.cpp
@@ -8,64 +8,56 @@
 #define MAX_SIZE 100000
 using namespace std;
 
-class Fibonacci{
-public:
+class Fibonacci {
+ public:
     /*
-     * m = modulo parameter
-     * p = p > q and p =< initSize
-     * q = q<p and q =< initSize
+     * modulo = modulo parameter
+     * pParam = pParam > qParam and pParam =< initSize
+     * qParam = qparam < pParam and qParam =< initSize
      */
-    Fibonacci (int m, int p, int q)
-            : m(m), p(p), q(q) {}
+    Fibonacci(int modulo, int pParam, int qParam)
+            : modulo(modulo), pParam(pParam), qParam(qParam) {}
     /*
      * Default constructor
      */
-    Fibonacci ():m(1),p(1),q(1){}
+    Fibonacci() : modulo(1), pParam(1), qParam(1) {}
+
+    void set_m(int modulo){
+        this->modulo = modulo;
+    }
+
+    void set_p(int pParam){
+        this->pParam = pParam;
+    }
+
+    void set_q(int qParam){
+        this->qParam = qParam;
+    }
     /*
      * rands = array with seed elements
      * randsNumber = quantity of numbers to generate
      * initSize = number of seed elements
      */
-    /*
-     * setter for m parameter
-     */
-    void set_m(int m){
-        this->m = m;
-    }
-    /*
-     * setter for p parameter
-     */
-    void set_p(int p){
-        this->p = p;
-    }
-    /*
-     * setter for q parameter
-     */
-    void set_q(int q){
-        this->q = q;
-    }
-
-    void get_rands(int* rands,int randsNumber, int initSize) {
-        if (this->p > initSize or this->q > initSize)
+    void rand(int* rands,int randsNumber, int initSize) {
+        if (this->pParam > initSize or this->qParam > initSize)
             throw std::invalid_argument("initSize bigger then p or q parameter");
         for (int i = initSize; i < randsNumber; i++) {
-            rands[i] = (rands[i-this->p] * rands[i-this->q] ) % this -> m ;
+            rands[i] = (rands[i-this->pParam] * rands[i-this->qParam] ) % this -> modulo;
         }
     }
 private:
-    int m,p,q;
+    int modulo,pParam,qParam;
 
 };
 
 
-
-int main(){
+int main() {
     int a[MAX_SIZE] = {7,16,5};
     Fibonacci rng = Fibonacci(17,3,1);
     rng.set_q(2);
-    rng.get_rands(a, SIZE, 3);
+    rng.rand(a, SIZE, 3);
     for (int i = 0 ; i < SIZE ; i++)
         cout << a[i] << " ";
-    cout << endl;
+    cout <<  3 / 4 << endl;
     return 0;
 }
