74006db1ece9b2b3ae0d91ab4d5bea636826cbd1
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2015-11-19 17:39:52 +0100

    Fixed build script.
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

--- exercise3/scripts/build.sh
+++ exercise3/scripts/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p ../build
 rm -f ../build/*
-LD_LIBRARY_PATH="/usr/local/lib" g++ -std=++11 ../src/exercise3.cpp -o ../build/generator
+LD_LIBRARY_PATH="/usr/local/lib" g++ --std=c++11 ../src/exercise3.cpp -o .
+./build/generator
 chmod +x ../build/generator
