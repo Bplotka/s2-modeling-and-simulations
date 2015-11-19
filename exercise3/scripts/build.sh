9ae68568158258260e2d1b436d039c7ab14cd0e8
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2015-11-19 17:51:04 +0100

    Fix newline.
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

--- exercise3/scripts/build.sh
+++ exercise3/scripts/build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 mkdir -p ../build
 rm -f ../build/*
-LD_LIBRARY_PATH="/usr/local/lib" g++ --std=c++11 ../src/exercise3.cpp -o .
-./build/generator
+LD_LIBRARY_PATH="/usr/local/lib" g++ --std=c++11 ../src/exercise3.cpp -o ../build/generator
 chmod +x ../build/generator
