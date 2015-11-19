3c0a14d59b6064817b0d72e8729b8c58d550d51c
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2015-11-19 10:26:32 +0100

    Added Cmake lists. Refactored script. Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

--- exercise3/src/exercise3.cpp
+++ exercise3/src/exercise3.cpp
@@ -1,12 +1,37 @@
-//
-// Created by pprokop on 11/17/15.
-//
+#include <bitset>
+#include <getopt.h>
+#include <iostream>
+#include <stdexcept>
+#include <stdint.h>
+#include <string>
+#include <vector>
+#include <stdlib.h>
 
-#include<iostream>
-#include<bitset>
 #define SIZE 80
 #define MAX_SIZE 100000
+
 using namespace std;
+
+/**
+ * Prokop, Plotka, Gondek Random Number Generator
+ */
+class PPGGenerator {
+ public:
+  explicit PPGGenerator(
+    const vector<int64_t>& _seed, int64_t _pParam, int64_t _qParam)
+    : seed(_seed), pParam(_pParam), qParam(_qParam) {}
+
+  virtual void rand() {
+
+  }
+
+ protected:
+  int64_t pParam;
+  int64_t qParam;
+  vector<int64_t> seed;
+};
+
+// TODO(bplotka) Refactor these classes!
 
 class Fibonacci {
  public:
@@ -50,6 +75,7 @@ private:
 
 };
 
+
 class Tausworth {
  public:
     Tausworth(int pParam, int qParam)
@@ -71,16 +97,16 @@ class Tausworth {
      * randsNumber = quantity of numbers to generate
      * bitSize = size of output elements
      */
-    void rand(int* rands,int randsNumber, int bitSize) {
+    void rand(int rands[],int randsCount, int bitSize) {
 
         int bitCount = get_bits(rands[0]);
         bitset<1000000> binarySeed(rands[0]);
-        for (int i = bitCount; i < randsNumber*bitSize; i++) {
+        for (int i = bitCount; i < randsCount*bitSize; i++) {
             binarySeed[i] = (binarySeed[i-this->pParam] == binarySeed[i-this->qParam]) ? 0 : 1;
         }
         int bitPosition = 0;
 
-        for (int i =0; i < randsNumber;i++ ) {
+        for (int i =0; i < randsCount;i++ ) {
             int result = 0;
             for(int j=0;j< bitSize; j++) {
                 result += (int) (binarySeed[bitPosition] << j);
@@ -104,8 +130,52 @@ private:
 };
 
 
+void help() {
+  cout << "Usage: " << "-t <generator type> -p <p param> -q <q param> -b "
+                         "<range begin> -e <range end> -h help";
+}
 
-int main() {
+int main(int argc, char **argv) {
+  string generatorType = "Default";
+  int64_t pParam = 1;
+  int64_t qParam = 1;
+  int64_t range[2] = {0, 10000000};
+  int c;
+
+  opterr = 0;
+  while ((c = getopt (argc, argv, "t:p:q:b:e:h")) != -1)
+    switch (c)
+    {
+      case 't':
+        generatorType = optarg;
+        break;
+      case 'p':
+        pParam = atoi(optarg);
+        break;
+      case 'q':
+        qParam = atoi(optarg);
+        break;
+      case 'b': // range begin.
+        range[0] = atoi(optarg);
+        break;
+      case 'e': // range end.
+        range[1] = atoi(optarg);
+        break;
+      case 'h': // range end.
+        help();
+        return 0;
+      case '?':
+        if (optopt == 't' || optopt == 'p' || optopt == 'q')
+          cerr << "Option -"<< optopt <<" requires an argument." << endl;
+        else
+          cerr << "Unknown option -"<< optopt <<" ." << endl;
+        return 1;
+
+      default:
+        help();
+        abort();
+    }
+
     int a[MAX_SIZE] = {7,16,5};
     int b[MAX_SIZE] = {31};
     Fibonacci rng = Fibonacci(17,3,1);
